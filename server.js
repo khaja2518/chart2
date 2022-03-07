ef641e0d51a60f350b6c1db748168107f20b8a73
khaja pasha <khajapasha.shek@praudyogi.com>
2022-03-07 12:50:27 +0530

    jf

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const path=require('path')
 
 const app = express()
-app.use(express.static(__dirname+'/dist/chart'))
+app.use(express.static(__dirname+'./dist/chart'))
 app.get('/*',(req,res)=>{
-    res.sendFile('index.html',{root:'src'})
+    res.sendFile('index.html',{root:'src/'})
 })
 
 app.listen(4000,()=>{
