0adc3c1a9f06ba7664a355d08aa9af9a0ec7ec50
khaja pasha <khajapasha.shek@praudyogi.com>
2022-03-07 12:57:52 +0530

    d,fhdkjf

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const path=require('path')
-
+const cors = require('cors')
 const app = express()
+
+app.all('/*', cors())
+
 app.use(express.static(__dirname+'./dist/chart'))
 app.get('/*',(req,res)=>{
     res.sendFile('index.html',{root:'src/'})
